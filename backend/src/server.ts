f4ce2dda69a8e694189f389ec37e2b5bc4433306
Dhallyson <dhallysonsilva1@gmail.com>
2026-03-11 11:09:48 -0300

    Instalação do typeorm

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,17 @@
-import express from "express";
+import 'reflect-metadata';
+import express from 'express';
+import routes from './routes';
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Servidor rodando com TypeScript 🚀");
 });
+
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
